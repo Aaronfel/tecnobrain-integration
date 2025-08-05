1b34a6e62e7be52e36a4060e6f48966841d9ea12
Aaronfel <feldmanaaron406@gmail.com>
2025-08-05 11:11:14 -0300

    change webhook for chat webhook

--- src/kommo/kommo.controller.ts
+++ src/kommo/kommo.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Query, Res, HttpStatus } from '@nestjs/common';
 import { KommoService } from './kommo.service';
-import { KommoWebhookDto } from './dto/kommo-message-received.dto';
+// import { KommoWebhookDto } from './dto/kommo-message-received.dto';
+import { Response } from 'express';
 // import { CreateKommoDto } from './dto/create-kommo.dto';
 
 @Controller('kommo')
@@ -8,7 +9,13 @@ export class KommoController {
   constructor(private readonly kommoService: KommoService) {}
 
   @Post('/webhook')
-  processWebhook(@Body() createKommoDto: KommoWebhookDto) {
-    return this.kommoService.processMessage(createKommoDto);
+  processWebhook(
+    @Query('scope_id') scopeId: string,
+    // @Body() createKommoDto: KommoWebhookDto,
+    @Res() res: Response,
+  ) {
+    console.log('scopeId', scopeId);
+    return res.status(HttpStatus.OK).send('Webhook received');
+    // return this.kommoService.processMessage(createKommoDto);
   }
 }
